f15309bd22b103195a6066595ed0ff047a49faa7
Ezhil-ezy <ezhil2pointo@gmail.com>
2024-05-07 19:53:27 +0530

    oops

---
+++ objects.py
@@ -0,0 +1,18 @@
+# objects....
+
+'''
+In program everthing is an object. 
+
+i. objects consists of two things. 
+  --> attributes (data, variables).
+  --> behaviour  (functions, methods).
+  --> objects is an instance of the class which has attributes and methods.
+
+'''
+
+# classes....
+
+'''
+--> class is (' blueprint or design ') of an object.
+
+'''
